313ab45fb4d43fcca486a0cc0ed95c41a40a0ac0
Juan Carlos Andresen <juan.carlos@andresen.mx>
2026-03-20 01:32:56 +0100

    Reapply "fix of rebooting due to the fact that task_safe_wire does not live in flash but in IRAM"
    This reverts commit 356a9244c036a529bb0a1b6f98414207f9ce317c.
    
    revert to the payload json format

--- sw/include/task_safe_wire.h
+++ sw/include/task_safe_wire.h
@@ -3,15 +3,15 @@
 
 #include <Arduino.h>
 
-void task_safe_wire_begin(uint8_t address);
-void task_safe_wire_lock();
-bool task_safe_wire_try_lock(uint32_t timeoutMs);
-void task_safe_wire_unlock();
-size_t task_safe_wire_write(uint8_t value);
-void task_safe_wire_restart();
-uint8_t task_safe_wire_request_from(uint8_t address, uint8_t quantity);
-int task_safe_wire_read();
-int task_safe_wire_available();
-uint8_t task_safe_wire_end();
+void   IRAM_ATTR task_safe_wire_begin(uint8_t address);
+void   IRAM_ATTR task_safe_wire_lock();
+bool   IRAM_ATTR task_safe_wire_try_lock(uint32_t timeoutMs);
+void   IRAM_ATTR task_safe_wire_unlock();
+size_t IRAM_ATTR task_safe_wire_write(uint8_t value);
+void   IRAM_ATTR task_safe_wire_restart();
+uint8_t IRAM_ATTR task_safe_wire_request_from(uint8_t address, uint8_t quantity);
+int    IRAM_ATTR task_safe_wire_read();
+int    IRAM_ATTR task_safe_wire_available();
+uint8_t IRAM_ATTR task_safe_wire_end();
 
 #endif
